ae7f155283e772f0d92e288e637ca787aaec0f7e
Ashwani <aksingh99903@gmail.com>
2019-08-06 05:45:38 +0530

    Added: find second largest number in array

--- array/program-to-find-second-largest-number-in-array.cpp
+++ array/program-to-find-second-largest-number-in-array.cpp
@@ -7,7 +7,19 @@ using namespace std;
 int main(){
 	int a[10] = {10,5,90,40,54,27,23,94,83,39};
 
-	int first, second = INT_MIN;
+	int largest = INT_MIN;
 
-	cout<<first<<endl<<second;
+	int second_largest = INT_MIN;
+
+	for(int i=0;i<9;i++){
+
+		if(a[i]>largest){
+			second_largest = largest;
+			largest = a[i];
+		}else if(a[i]>second_largest && a[i]!=largest){
+			second_largest = a[i];
+		}
+	}
+
+	cout<<second_largest;
 }
