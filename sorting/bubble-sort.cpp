6932b01a38d07b5d45d5d1e0aeaddfe31338068a
Ashwani <imakumar98@gmail.com>
2019-10-02 10:51:10 +0530

    bubble sort done

--- sorting/bubble-sort.cpp
+++ sorting/bubble-sort.cpp
@@ -2,21 +2,42 @@
 
 using namespace std;
 
+void bubbleSort(int[], int);
+
 int main(){
 	
 	int arr[5] = {8,5,7,3,2};
-	int temp;
+	
 
-	for(int i=0; i<arr.size(); i++){
-		if(arr[i]>arr[i+1]){
-			temp = arr[i];
-			arr[i] = arr[i+1];
+
+
+
+	int length = sizeof(arr)/sizeof(arr[0]);
+
+	bubbleSort(arr, length);
+
+
+	
+}
+
+
+void bubbleSort(int arr[], int n){
+	int number_of_comparisons = 0;
+	int temp = 0;
+	for(int i=0; i<n-1;i++){
+		for(int j=0; j<n-1-i; j++){
+			if(arr[j]>arr[j+1]){
+				number_of_comparisons++;
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
 		}
 	}
 
-
-	//Print all the elements
-	for(int i=0; i<arr.size(); i++){
-		cout<<arr[i]<<endl;
+	for(int i=0; i<n;i++){
+		cout<<arr[i];
 	}
+
+	cout<<"Number of comparison : "<<number_of_comparisons;
 }
